69fbd6e2cdedbdbe8fdbc6bc4cd4e36ecc61a9e8
Rami Hawi <ramihawi7@gmail.com>
2023-07-10 09:51:28 -0700

    @draw

--- Draw.py
+++ Draw.py
@@ -1,2 +1,23 @@
-for i in range(6):
-    print("ssaa")
+from gasp import *
+
+begin_graphics()
+
+
+
+Line((300,210),(290,190))
+Line((290,190),(310,190))
+Circle((300,200),40)
+Circle((285,210),5)
+Circle((315,210),5)
+Arc((300, 200), 30, 225, 315)
+
+
+
+update_when('key_pressed')  
+end_graphics()
+drawstick(200,200)
+
+
+
+
+    
